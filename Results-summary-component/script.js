d59e1e300b95034e2163ff67635843ad18fe2e71
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2024-06-24 20:54:59 +0100

    Complete the bonus JS challenge

--- Results-summary-component/script.js
+++ Results-summary-component/script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+
+async function getUserResults() {
+    const rawData = await fetch("./data.json");
+    const results = await rawData.json();
+
+    return results;
+}
+
+getUserResults().then((data) => displayResults(data));
+
+function displayResults(userResults) {
+    let summarySectionHTML = "";
+
+    userResults.forEach(({ category, score, icon }) => {
+        const newCriteria = `
+        <div class="criteria">
+        <p class="label">
+            <img src=${ icon } alt="icon">
+            ${ category }
+        </p>
+        <p><span class="grade">${ score }</span> <span>/</span> <span> 100</span></p>
+        </div>
+        `
+
+        summarySectionHTML += newCriteria;
+    })
+
+    const gradesPanel = document.querySelector(".grades");
+    gradesPanel.innerHTML = summarySectionHTML;
+}
